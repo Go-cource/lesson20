ba9333305abc5d6cf912e08dbd197bbada67aab3
Fisherman August <fshrmn.august@gmail.com>
2025-12-18 19:47:09 +0300

    with go server

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"text/template"
+)
 
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("pages/index.html")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	tmpl.Execute(w, nil)
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("Error: ", err)
+	}
 }
